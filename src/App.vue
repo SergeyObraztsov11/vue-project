e54a3e0b6b38c9b595f92f946fe0de988f9e821a
Sergey <markrut23@mail.ru>
2024-10-14 23:56:06 +0400

    start

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {useTestStore} from "@stores/testStore.js"
+const authStore = useTestStore();
+
+</script>
+
+<template>
+    <div>
+        token: {{authStore.getToken()}}
+        <button @click="authStore.logout">Logout</button>
+        <router-view />
+    </div>
+</template>
+
+<style scoped></style>
