6bb346d0feb7b773a9450a785602c91087d99e5e
Sergey <markrut23@mail.ru>
2024-10-15 00:43:17 +0400

    .

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
 import { useTestStore } from "@stores/testStore.js";
-import { ref } from "vue";
+import {  ref } from "vue";
 const authStore = useTestStore();
 const token = ref(null)
+
+
 
 </script>
 
 <template>
     <div>
-        <h1 v-if="token.value">token: {{ authStore.getToken() }}</h1>
+        <h1 v-if="token">token: {{ authStore.getToken() }}</h1>
         <h1 v-else>О, нет 😢</h1>
 
         <button @click="authStore.logout">Logout</button>
